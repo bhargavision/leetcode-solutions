8ee2504f824d22a881f7acf3207f4dddbff6f8cb
bhargavision <voobhargavi5@gmail.com>
2026-04-05 22:00:20 +0530

    Create 238_product_of_array_except_itself.java

---
+++ medium/238_product_of_array_except_itself.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+class product_of_array_except_itself {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
+        int[] result = productExceptItself(nums);
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    static int[] productExceptItself(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] = res[i] * right;
+            right = right * nums[i];
+        }
+        return res;
+    }
+}
